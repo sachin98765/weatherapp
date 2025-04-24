be32ce9226202ad41f25b7fd223860f4bdbc4ad4
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-04-25 01:16:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
 import { FaSearch } from "react-icons/fa"
 import { WiHumidity } from "react-icons/wi"
 import { WiStrongWind } from "react-icons/wi"
-import wicon from "../src/assets/wicon.png"
+import axios from "axios"
 
 const App = () => {
+  const [search, setSearch] = useState("")
+  const [loading,setLoading] = useState(false)
+  const [temperature,setTemperature] = useState(null)
+  const [humidity,setHumidity] = useState(null)
+  const [windSpeed,setWindSpeed] = useState(null)
+  const [cityName,setCityName]=useState("")
+  const [weatherIcon, setWeatherIcon] = useState("01d")
+
+  const API_KEY = "65b5406f81721a3a308282e6d90b5773"
+  const fetchWeather =async()=>{
+    // console.log(search)
+    if(!search) return;
+    setLoading(true)
+    try{
+      const {data} =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${API_KEY}`)
+      console.log(data)
+      if(data.cod===200){
+        setTemperature(data.main.temp)
+        setHumidity(data.main.humidity)
+        setWindSpeed(data.wind.speed)
+        setCityName(data.name)
+        setWeatherIcon(data.weather[0].icon)
+
+      }
+    }catch(error){
+      console.log(error)
+      setCityName("city not found")
+      setTemperature(null)
+      setHumidity(null)
+      setWindSpeed(null)
+      setWeatherIcon("01d")
+    }
+    setLoading(false)
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-green-950 to-black text-white">
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-600 to-pink-600 from-green-950 to-black text-white">
       {/* Serch bar and icon */}
       <div className="flex items-center bg-white rounded-full px-4 py-2 mb-6 w-80 shadow-lg">
-        <input type="text" placeholder="Search" className="flex-1 text-black outline-none px-2"/>
-        <FaSearch className="text-gray-500  cursor-pointer" />
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+          className="flex-1 text-black outline-none px-2"
+        />
+        <FaSearch onClick={fetchWeather} className="text-gray-500  cursor-pointer" />
       </div>
 
       {/* Wetaher icon */}
-      <img src={wicon} alt="" className="w-20 h-20 mb-4" />
+      <img src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="" className="w-20 h-20 mb-4" />
 
       {/* temprature and Wind Speed*/}
-      <h1 className="text-4xl font-bold">40°C</h1>
-      <h2 className="text-2xl mt-2 font-semibold">Kolkata</h2>
+      <h1 className="text-4xl font-bold">{loading?"loading...":temperature!==null?`${temperature}°C`:"--"}</h1>
+      <h2 className="text-2xl mt-2 font-semibold">{cityName || "type to check temperature"}</h2>
 
       {/* humidity and city name */}
       <div className=" w-full max-w-md mt-67 flex flex-col md:flex-row items-center justify-between md:items-start">
         <div className="flex flex-col items-center ">
           <WiHumidity className="text-3xl" />
-          <span className="text-lg font-medium">80%</span>
+          <span className="text-lg font-medium">{humidity!==null?`${humidity}%`:"--"} </span>
           <p className="text-sm">Humidity</p>
         </div>
         <div className="flex flex-col items-center">
           <WiStrongWind className="text-3xl" />
-          <span className="text-lg font-medium">10 km/h</span>
+          <span className="text-lg font-medium"> {windSpeed!==null?`${windSpeed}km/h`:"--"} </span>
           <p className="text-sm">Wind Speed</p>
         </div>
       </div>
