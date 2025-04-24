1e1ab44a0dd79b6ee66ed1b5e4cd5b834341a8d9
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-04-24 23:13:16 +0530

    bulid weather app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
+import { WiStrongWind } from "react-icons/wi";
+
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder='Search' />
+        <FaSearch/>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
